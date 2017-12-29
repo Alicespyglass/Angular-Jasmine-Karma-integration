2d8866c6b81ea26b294a16dda97e0b87476f86e4
alicespyglass <alicespyglass@gmail.com>
2017-12-29 14:01:44 +0000

    integration setting - write passing tests for 1 - chekcing dom span text and 2 - element property

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { VoterComponent } from './voter.component';
 
@@ -19,6 +20,26 @@ describe('VoterComponent', () => {
 
   });
 
-  it('', () => {
+  fit('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges(); // explicitly tell Angular to detect changes and update Dom
+
+    // as well as css selector, could query by directive e.g. By.directive(VoterComponent)
+    // could also do queryAll which returns all the elements which match this predicate
+    let de = fixture.debugElement.query(By.css('.vote-count')); // de = debugElement
+    let el: HTMLElement = de.nativeElement; // nativeElement is of type 'any' so define as a HTML element first
+
+    // Assert
+    expect(el.innerText).toContain('21'); // innerText as we're interested in the span
   });
+
+  fit('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
+  })
 });
