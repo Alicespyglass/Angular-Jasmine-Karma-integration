901120157c61125e6d9d10e1967bff5f5da8f846
Alice Cheung <alice@myurbanjungle.com>
2018-01-02 13:50:00 +0000

    integration testing - 4 - testing navigation and routes

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -15,6 +15,7 @@ class RouterStub {
 }
 
 class ActivatedRouteStub {
+    // remember to initialise by setting as an empty observable
     params: Observable<any> = Observable.empty();
 }
 
@@ -22,28 +23,42 @@ describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ],
-      // replace router class with RouterStub
-      // supply an object instead with 2 properties
-      // instructs angular that when it sees Router as one of the parameters
-      // in the constructor of a component, to use an instance of Stopclass  RouterStub instead
-      providers: [
-        { provide: Router, useClass: RouterStub },
-        { provide: ActivatedRoute, useClass: ActivatedRouteStub }
-      ]
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          declarations: [ UserDetailsComponent ],
+          // replace router class with RouterStub
+          // supply an object instead with 2 properties
+          // instructs angular that when it sees Router as one of the parameters
+          // in the constructor of a component, to use an instance of Stopclass  RouterStub instead
+          providers: [
+            { provide: Router, useClass: RouterStub },
+            { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+          ]
     })
     .compileComponents();
-  }));
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserDetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  fit('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    fit('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    fit('should redirect the user to the users page after saving', () => {
+        // 1 - want to spyOn the navigate method of the Router
+        let router = TestBed.get(Router);
+        let spy = spyOn(router, 'navigate');
+
+        // Act
+        component.save();
+
+        // Assert
+        expect(spy).toHaveBeenCalledWith(['users']); // users is the object that we should pass to navigate method
+
+    })
+
 });
