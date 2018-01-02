4477b728f89da7453fc7b47856a817bee2fd4e62
Alice Cheung <alice@myurbanjungle.com>
2018-01-02 15:01:48 +0000

    integration testing - 3 - Testing router-outlet components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
-/* tslint:disable:no-unused-variable */
+ /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing'; 
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -12,7 +14,8 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+        imports: [ RouterTestingModule.withRoutes([]) ],
+        declarations: [ AppComponent ]
     })
     .compileComponents();
   }));
@@ -20,6 +23,27 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges(); 
+    fixture.detectChanges();
   });
+
+  fit('should have a router-outlet', () => {
+      // get reference to router-outlet element
+      let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+      // Assert
+      expect(de).not.toBeNull();
+  });
+
+  fit('should have a link to todos page', () => {
+      // get reference to router-link element
+      // plural naming for queryAll - expecting lots of links to search to find todos
+      let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+      // expect it to be true if index comes back >-1
+      // pass in findIndex predicate - looking in properties property of href
+      let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
+
+      // Assert
+      expect(index).toBeGreaterThan(-1);
+  })
 });
